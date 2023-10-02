602804c78d166af776270cc1fb141d8c887b0251
MandimbisoaMiarinjato <mandimbisoairiana@gmail.com>
2023-10-02 10:04:12 +0300

    modif sql

--- projet.1.sql
+++ projet.1.sql
@@ -6,4 +6,15 @@ CREATE TABLE `admin` (
  `phone` varchar(255) NOT NULL,
  PRIMARY KEY (`iid_admin`),
  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+CREATE TABLE `products` (
+ `id_product` int NOT NULL AUTO_INCREMENT,
+ `p_name` varchar(255) NOT NULL,
+ `p_quantity` int NOT NULL,
+ `p_price` int NOT NULL,
+ `p_image` varchar(255) NOT NULL,
+ PRIMARY KEY (`id_product`),
+ UNIQUE KEY `p_name` (`p_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
